7c4f12eb7e244121b83c22c4520fb6d64a549f32
bkappiah007@gmail.com <bkappiah007l.com>
2026-09-14 22:10:02 -0500

    Update Quarto website

--- _extensions/quarto-ext/fontawesome/fontawesome.lua
+++ _extensions/quarto-ext/fontawesome/fontawesome.lua
@@ -1,11 +1,14 @@
+
+
+
 local function ensureLatexDeps()
-  quarto.doc.use_latex_package("fontawesome6")
+  quarto.doc.use_latex_package("fontawesome5")
 end
 
 local function ensureHtmlDeps()
   quarto.doc.add_html_dependency({
     name = 'fontawesome6',
-    version = '6.7.2',
+    version = '1.2.0',
     stylesheets = {'assets/css/all.min.css', 'assets/css/latex-fontsize.css'}
   })
 end
@@ -72,15 +75,10 @@ return {
     -- detect pdf / beamer / latex / etc
     elseif quarto.doc.is_format("pdf") then
       ensureLatexDeps()
-      local options = ""
-      if (group == "regular") then
-        options = "[regular]"
-      end
-      local icons = "\\faIcon" .. options .. "{" .. icon .. "}"
       if isEmpty(isValidSize(size)) then
-        return pandoc.RawInline('tex', icons)
+        return pandoc.RawInline('tex', "\\faIcon{" .. icon .. "}")
       else
-        return pandoc.RawInline('tex', "{\\" .. size .. icons .. "}")
+        return pandoc.RawInline('tex', "{\\" .. size .. "\\faIcon{" .. icon .. "}}")
       end
     else
       return pandoc.Null()
